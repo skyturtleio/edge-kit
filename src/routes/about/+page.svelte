56641fe68f76103410760991aa5265c6c9d85928
Leo Mendoza Jr <dev@skyturtle.io>
2023-06-30 16:43:48 -0700

    add about, blog pages

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,3 @@
-<nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
-</nav>
 <h1>About Page</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>hello world</p>
